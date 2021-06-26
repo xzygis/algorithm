dff565ee6cbfeb1375dd5ca9985a717cd4b78ed4
eagle <crackcosmas@gmail.com>
2021-06-26 18:28:48 +0800

    find mininum in rotated sorted array

---
+++ sword/_11_find-minimum-in-rotated-sorted-array/solution.go
@@ -0,0 +1,15 @@
+package _11_find_minimum_in_rotated_sorted_array
+
+func findMin(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+	for left < right {
+		mid := left + (right - left) / 2
+		if nums[mid] < nums[right] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return nums[left]
+}
