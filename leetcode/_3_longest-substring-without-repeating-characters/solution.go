7d3e6066e9f7c19ddcde49e460dc26f39a6ddb4b
eagle <crackcosmas@gmail.com>
2024-03-13 19:48:02 +0800

    "无重复字符的最长子串": 增加一种解法

--- leetcode/_3_longest-substring-without-repeating-characters/solution.go
+++ leetcode/_3_longest-substring-without-repeating-characters/solution.go
@@ -3,6 +3,24 @@ package _3_longest_substring_without_repeating_characters
 func lengthOfLongestSubstring(s string) int {
 	m := map[byte]int{}
 	l := 0
+	r := 0
+	ans := 0
+	for r < len(s) {
+		if _, ok := m[s[r]]; ok {
+			l = max(l, m[s[r]]+1)
+		}
+
+		m[s[r]] = r
+		ans = max(ans, r-l+1)
+		r++
+	}
+
+	return ans
+}
+
+func lengthOfLongestSubstringV1(s string) int {
+	m := map[byte]int{}
+	l := 0
 	r := -1
 	ans := 0
 	for l < len(s) {
