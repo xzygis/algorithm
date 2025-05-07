be8236a0f40e58208ee9eea29eecaef356a98368
eagle <crackcosmas@gmail.com>
2025-05-08 00:47:03 +0800

    增加：48 旋转图像
    Change-Id: I9cb43ea6cb10b8c59192e3809011151be039e2ad

---
+++ leetcode/_48_rotate-image/solution.go
@@ -0,0 +1,32 @@
+package _54_spiral_matrix
+
+// [i][j] => [j][col-i-1]
+// [i][j] = [col-j-1][i]
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
+			temp := matrix[i][j]
+			matrix[i][j] = matrix[n-j-1][i]
+			matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+			matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+			matrix[j][n-i-1] = temp
+		}
+	}
+}
+
+// [i][j] => [j][col-i-1]
+func rotateV1(matrix [][]int) {
+	temp := make([][]int, len(matrix))
+	for i := 0; i < len(matrix); i++ {
+		temp[i] = make([]int, len(matrix[0]))
+	}
+
+	for i, row := range matrix {
+		for j, v := range row {
+			temp[j][len(matrix)-i-1] = v
+		}
+	}
+
+	copy(matrix, temp)
+}
---
+++ leetcode/_48_rotate-image/solution_test.go
@@ -0,0 +1,26 @@
+package _54_spiral_matrix
+
+import (
+	"testing"
+
+	. "github.com/bytedance/mockey"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/xzygis/algorithm/leetcode/utils"
+)
+
+func Test_rotate(t *testing.T) {
+	PatchConvey("Test rotate image", t, func() {
+		matrix := utils.ParseMatrix("[[1,2,3],[4,5,6],[7,8,9]]")
+		expected := utils.ParseMatrix("[[7,4,1],[8,5,2],[9,6,3]]")
+		rotate(matrix)
+		So(matrix, ShouldResemble, expected)
+	})
+
+	PatchConvey("Test rotate image", t, func() {
+		matrix := utils.ParseMatrix("[[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]")
+		expected := utils.ParseMatrix("[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]")
+		rotate(matrix)
+		So(matrix, ShouldResemble, expected)
+	})
+
+}
